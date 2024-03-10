2b954e3bba6e2e0c33d32f9701235c4c3676c26d
José Abelardo Vicente Filho <55700120+maxabelardo@users.noreply.github.com>
2024-03-10 16:49:03 -0300

    update

--- core/views.py
+++ core/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.contrib import messages
+from django.shortcuts import redirect
 
-from .forms import ContatoForm
-
+from .forms import ContatoForm, ProdutoModelForm
+from .models import Produto
 
 def index(reguest):
     return render(reguest, 'index.html')
@@ -13,29 +14,42 @@ def contato(reguest):
 
     if str(reguest.method) == 'POST':
         if form.is_valid():
-            nome = form.cleaned_data['nome']
-            email = form.cleaned_data['email']
-            assunto = form.cleaned_data['assunto']
-            mensagem = form.cleaned_data['mensagem']
-            
-            print('Mensagem envidada ')
-            print(f'Nome: {nome}')
-            print(f'Nome: {email}')
-            print(f'Nome: {assunto}')
-            print(f'Nome: {mensagem}')
-            
-            messages.success(reguest,  'E-mail enviado com sucesso!')
+            form.send_mail()
+
+            messages.success(reguest, 'E-mail enviado com sucesso!')
             form = ContatoForm()
         else:
-            messages.error(reguest, 'Erro ao enviar o e-mail')            
-
+            messages.error(reguest, 'Erro ao enviar e-mail')
     context = {
         'form': form
     }
-
-    return render(reguest,'contato.html', context)
+    return render(reguest, 'contato.html', context)
 
 
 def produto(request):
-    return render(request,'produto.html')
+    if str(request.user) != 'AnonymousUser':
+        if str(request.method) == 'POST':
+            form = ProdutoModelForm(request.POST, request.FILES)
+            if form.is_valid():
+                form.save()
 
+                #prod = form.save(commit=False)
+                #print(f'Nome: {prod.nome}')
+                #print(f'Preço:{prod.preco}')
+                #print(f'Estoque:{prod.estoque}')
+                #print(f'imagem:{prod.imagem}')
+
+
+                messages.success(request, 'Produto salvo com sucesso.')
+
+                form = ProdutoModelForm()
+            else:
+                messages.error(request, 'Erro ao salvar produto.')
+        else:
+            form = ProdutoModelForm()
+        context = {
+            'form': form
+        }
+        return render(request, 'produto.html', context)
+    else:
+        return redirect('index')
